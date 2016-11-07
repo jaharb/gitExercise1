3eded5d8d0731af213e87a7cf8c4f1a8d28fcff0
Andy Harbert <andy.harbert@oc.edu>
2016-11-07 12:20:08 -0600

    B changes

--- gitExercise1/Program.cs
+++ gitExercise1/Program.cs
@@ -10,7 +10,7 @@ namespace gitExercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello from Main!");
+            Console.WriteLine("hello from Main!"); //goodbye
             MyClass.hello();
             
         }
